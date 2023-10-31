6589eea605ee628e203f883ec2dea2c4ab09aeb9
Bergrebell <rrkuepper@gmail.com>
2023-10-31 14:06:47 +0100

    add: dynamic content to template

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
+require 'erb'
+
 class App
   def call(env)
     headers = { 'Content-Type' => 'text/html' }
 
-    response_html = File.read('views/index.html')
+    title = "Roman"
+    erb = ERB.new(html_template)
+    response_html = erb.result(binding)
 
     # same response, i.e. contents of index.html
     # is sent no matter the request
     [200, headers, [response_html]]
+  end
+
+  def html_template
+    File.read 'views/index.html.erb'
   end
 end
