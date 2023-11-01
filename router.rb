0462f8756958492da7e7b7cca80dec8b32283389
Bergrebell <rrkuepper@gmail.com>
2023-11-01 16:08:44 +0100

    refactor: get method in router to prevent path from being overidden

--- router.rb
+++ router.rb
@@ -26,12 +26,10 @@ class Router
       @routes[path] = blk
     else
       if path.include? '/'
-        controller, action = path.split('/')
-        controller_klass_name = controller.capitalize + 'Controller'
-        controller_klass = Object.const_get(controller_klass_name)
-
-        @routes[path.prepend('/')] = ->(env) {
-          controller_klass.new(env).send(action.to_sym)
+        @routes['/' + path] = ->(env) {
+          controller_name, action_name = path.split('/')
+          controller_klass = constantize(controller_name)
+          controller_klass.new(env).send(action_name.to_sym)
         }
       end
     end
@@ -41,5 +39,12 @@ class Router
     path = env['REQUEST_PATH']
     handler = @routes[path] || ->(env) { "no route found for #{path}" }
     handler.call(env) # calls the lambda function from the routes hash
+  end
+
+  private
+
+  def constantize(name)
+    controller_klass_name = name.capitalize + 'Controller'
+    Object.const_get(controller_klass_name)
   end
 end
