59a3137e5e0e4d5799df261dd9bfeeedf22e2c99
Bergrebell <rrkuepper@gmail.com>
2023-11-01 12:19:53 +0100

    add: env hash to router

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Router.draw do
   get('/') {"Romans ruby blog"}
   get('/foo') {"fooohuuu"}
   get('/articles') { 'All Articles' }
-  get('/articles/1') { "First Article" }
+  get('/articles/1') do |env|
+    puts "Path: #{env['REQUEST_PATH']}"
+    "First article"
+  end
 end
