3e8f39c4247b9827a6d6a6d24557038c72e80b69
Bergrebell <rrkuepper@gmail.com>
2023-11-02 11:51:37 +0100

    add: article model

--- controllers/articles_controller.rb
+++ controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 require_relative 'application_controller'
+require_relative '../models/article'
 
 class ArticlesController < ApplicationController
   def index
     @title = 'Coming from a variable in the controller, baby!'
+    @article = Article.new("There is no Rails", "just plain ruby with some gems.")
   end
 end
