91321b1fa266500fb25c5158837f0b140e4d89cb
Bergrebell <rrkuepper@gmail.com>
2023-11-02 15:16:19 +0100

    add: middleware

--- app.rb
+++ app.rb
@@ -1,14 +1,9 @@
 require 'debug'
-require 'logger'
 require 'zeitwerk'
 require_relative './config/routes'
 
 class App
-  attr_reader :logger
-
   def initialize
-    @logger = Logger.new('log/development.log')
-
     loader = Zeitwerk::Loader.new
     loader.push_dir('models')
     loader.push_dir('controllers')
@@ -16,18 +11,11 @@ class App
   end
 
   def call(env)
-    logger.info "#{env['REQUEST_METHOD']} #{env['REQUEST_PATH']}"
-
     headers = { 'Content-Type' => 'text/html' }
 
     response_html = router.build_response(env)
 
     [200, headers, [response_html]]
-
-  rescue => e
-    logger.error("Error: #{e.message}")
-
-    [200, headers, ["Error: #{e.message}"]]
   end
 
   private
--- config.ru
+++ config.ru
@@ -1,10 +1,12 @@
 require 'rack'
-require_relative './app'
+require_relative 'app'
+require_relative 'middleware/logging'
 
 use Rack::Reloader, 0
 
 # Serve all requests beginning with /public  from the "public" folder
 use Rack::Static, urls: ['/public', "/favicon.ico"]
+use Logging
 
 # 'run' method is provided by rack. takes a ruby object as an argument and calls 'call' method on it.
 run App.new
